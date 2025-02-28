1c655f4c2d774d38de8b95ed7e4ed82691e37a43
Nicolas-Moises <nicolas.nicc21@gmail.com>
2025-02-28 13:47:39 -0300

    feat: add category page

---
+++ src/app/products/[category]/page.tsx
@@ -0,0 +1,50 @@
+'use client'
+import { useParams } from 'next/navigation'
+
+import { AppLayout, PageTitle } from '@/components/app-layout'
+import { ProductCard } from '@/components/products/product-card'
+import { ProductsEmpty } from '@/components/products/products-empty'
+import { ProductsError } from '@/components/products/products-error'
+import { ProductSkeleton } from '@/components/products/products-skeleton'
+import { LinkBack } from '@/components/ui/link-back'
+import { useGetProductsByCategory } from '@/hooks/use-get-products'
+
+export default function Category() {
+  const { category } = useParams<{ category: string }>()
+  const decodedCategory = decodeURIComponent(category)
+
+  const {
+    data: products,
+    isLoading: isProductsLoading,
+    isError: isProductsError,
+  } = useGetProductsByCategory(decodedCategory)
+
+  return (
+    <AppLayout>
+      <LinkBack path={'/products'} />
+      <div className="mt-6 space-y-3">
+        <PageTitle title={`Produtos na Categoria: ${decodedCategory}`} />
+        <p>Acesse gerencie a sua lista de produtos à venda</p>
+      </div>
+
+      <div className="col-span-2 mt-10 grid grid-cols-3 gap-x-6 gap-y-10">
+        {isProductsError ? <ProductsError /> : null}
+        {isProductsLoading
+          ? Array.from(
+              {
+                length: 10,
+              },
+              (_, index) => <ProductSkeleton key={index} />,
+            )
+          : null}
+        {products && products.length > 0 ? (
+          products.map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))
+        ) : (
+          <ProductsEmpty />
+        )}
+      </div>
+    </AppLayout>
+  )
+}
