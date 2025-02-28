11b0b261ce74bfc93fd4234c69c360475c2f80c6
Nicolas-Moises <nicolas.nicc21@gmail.com>
2025-02-28 13:34:18 -0300

    feat: add product types

---
+++ src/types/products.ts
@@ -0,0 +1,16 @@
+export type Category = string
+
+export type Rating = {
+  rate: number
+  count: number
+}
+
+export type Product = {
+  category: string
+  description: string
+  id: number
+  image: string
+  price: number
+  title: string
+  rating: Rating
+}
