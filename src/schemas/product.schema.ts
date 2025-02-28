1a6f727d32c54ddecf4befb0c3b4a4c6dfe643c3
Nicolas-Moises <nicolas.nicc21@gmail.com>
2025-02-28 13:37:49 -0300

    feat: add product schema to forms

---
+++ src/schemas/product.schema.ts
@@ -0,0 +1,30 @@
+import { z } from 'zod'
+
+export const productFormSchema = z.object({
+  category: z.string().min(3, 'A categoria é obrigatória'),
+  description: z
+    .string({
+      required_error: 'A descrição do produto é obrigatória.',
+    })
+    .min(2, 'A descrição do produto é obrigatória.'),
+  image: z
+    .string({
+      required_error: 'Insira uma URL válida',
+    })
+    .url({
+      message: 'Insira uma URL válida',
+    }),
+  price: z
+    .string({
+      required_error: 'O valor do produto é obrigatório.',
+    })
+    .min(2, 'O valor do produto é obrigatório.'),
+  title: z
+    .string({
+      required_error: 'O nome do produto é obrigatório.',
+    })
+    .min(2, 'O nome do produto é obrigatório.')
+    .max(30, 'O tílulo deve ter no máximo 30 caracteres.'),
+})
+
+export type ProductFormValues = z.infer<typeof productFormSchema>
